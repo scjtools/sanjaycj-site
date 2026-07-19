54c4b618b8266de662dfa04999f33433427b159f
Sanjay CJ <sanjaycj@live.com>
2026-07-19 23:48:04 +0400

    Switch back to static output (analytics stays enabled)

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sanjaycj.com',
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
